3fa98d2a845d5f300f97e858542ba10c0c27d010
Ajeet Murty <ajeetmurty.dev@gmail.com>
2012-11-30 01:13:49 -0500

    Signed-off-by: Ajeet Murty <ajeetmurty.dev@gmail.com>

--- LearnJava/src/com/adora/learn/java/LearnMain.java
+++ LearnJava/src/com/adora/learn/java/LearnMain.java
@@ -12,25 +12,11 @@ public class LearnMain {
 	}
 
 	public LearnMain(){
-		doit();
+		doSomething();
 	}
 	
 	private void doSomething(){
 		logp.log("start");
 		logp.log("stop");
-	}
-	
-	private void doit(){
-		Hashtable<Integer, String> tbl = new Hashtable<Integer, String>();
-		
-		tbl.put(new Integer(1), "ajeet");
-		tbl.put(new Integer(1), "ajeet");
-		tbl.put(new Integer(2), "phil");
-		
-		if(tbl.contains("ajeet")){
-			System.out.print("found");
-		}else{
-			System.out.print("fail");
-		}
 	}
 }
