11f51e8ca3a47e37fd149660f694d6f3f2c85804
Ajeet Murty <ajeetmurty.dev@gmail.com>
2013-03-11 02:51:46 -0400

    cleanup

--- RefJava/src/main/java/ajeetmurty/refcode/java/template/io/RJFile.java
+++
@@ -1,8 +0,0 @@
-package ajeetmurty.refcode.java.template.io;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class RJFile {
-	private final Logger logp = LoggerFactory.getLogger(this.getClass().getName());
-}
