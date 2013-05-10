a27db123e9e925f535d3a5b4736b851bb7463dfc
Ajeet Murty <ajeetmurty.dev@gmail.com>
2013-05-10 03:44:32 -0400

    adding code to detect host details.

--- python/src/ajeetmurty/template.py
+++ python/src/ajeetmurty/template.py
@@ -1,18 +1,27 @@
 import logging.config
 import os
-import sys
+import platform
 
 logging.config.fileConfig('../../resources/logging.conf')
 logr = logging.getLogger('pyLog')
 
 def main():
-    logr.info('start')
-    print_sys_info()
-    logr.info('stop')
+    try:
+        logr.info('start')
+        print_sys_info()
+        do_something()
+        return 0
+    except Exception: 
+        logr.exception('exception!')
+        return 1
+    finally:
+        logr.info('stop')
 
 def print_sys_info():
-    logr.info('login: '+ os.getlogin())
-    logr.info('os: '+ sys.platform)
+    logr.info('login|hostname|os|python: ' + os.getlogin() + '|' + platform.node() + '|' + platform.system() + '-' + platform.release() + '|' + platform.python_version())
+
+def do_something():
+    logr.info('doing something')
 
 if __name__ == '__main__':
     main()
