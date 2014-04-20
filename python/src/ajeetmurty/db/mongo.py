808cd4af324b3ae43066468c8144009f0006002f
Ajeet Murty <ajeetmurty.dev@gmail.com>
2014-04-20 02:26:02 -0400

    changing mongodb conn test code to include external config file parser.

--- python/src/ajeetmurty/db/mongo.py
+++ python/src/ajeetmurty/db/mongo.py
@@ -1,23 +1,26 @@
-import logging.config
 import os
 import platform
+import logging.config
+import configparser
 from pymongo import MongoClient
-from datetime import datetime
 
 logging.config.fileConfig('logging.conf')
 logr = logging.getLogger('pylog')
-mongo_ip = '192.168.0.104'
-mongo_port = 27017
-mongo_db = 'mongotest'
-mongo_collection = 'mongotesttable'
-mongo_token01 = 'testuser'
-mongo_token02 = 'testpassword'
+config = configparser.ConfigParser()
+config.read('config/mongodb_mongolab-test.ini')
+MONGODB_URI = config.get('connection_params', 'uri')
+MONGODB_HOST = config.get('connection_params', 'host')
+MONGODB_PORT = config.get('connection_params', 'port')
+MONGODB_DATABASE = config.get('connection_params', 'database')
+MONGODB_COLLECTION = config.get('connection_params', 'collection')
+MONGODB_TOKEN01 = config.get('connection_params', 'token01')
+MONGODB_TOKEN02 = config.get('connection_params', 'token02')
 
 def main():
     logr.info('start')
     try:
         print_sys_info()
-        do_mongo()
+        test_mongo_lab_conn()
     except Exception: 
         logr.exception('Exception')
     logr.info('stop')
@@ -25,28 +28,17 @@ def main():
 def print_sys_info():
     logr.info('login|hostname|os|python : {0}|{1}|{2}|{3}.'.format(os.getlogin(), platform.node() , platform.system() + '-' + platform.release() , platform.python_version()))
 
-def do_mongo():
-    out_dict = {}
-    out_dict['test_string'] = 'test01 value'
-    out_dict['test_int'] = 123456789
-    out_dict['test_string_int'] = int('123456789')
-    out_dict['test_float'] = 4.123456789
-    out_dict['test_string_float'] = float('4.123456789')
-    out_dict['test_date'] = datetime.utcnow()
-    out_dict['test_string_date'] = datetime.strptime('Mon Jan 13 16:35:17 2014', '%a %b %d %H:%M:%S %Y')
-    commit_to_db(out_dict)
-
-def commit_to_db(input_dict):
-    logr.info('committing this data: ' + str(input_dict))
-    client = MongoClient(mongo_ip, mongo_port)
-    logr.info('mongodb server info: ' + str(client.server_info()))
-    db = client[mongo_db]
-    db.authenticate(mongo_token01,mongo_token02)
-    coll = db[mongo_collection]
-    obj_id = coll.insert(input_dict)
-    logr.info('commit successful: ' + str(obj_id))
+def test_mongo_lab_conn():
+    conn_string = MONGODB_URI.format(MONGODB_TOKEN01, MONGODB_TOKEN02, MONGODB_HOST, MONGODB_PORT, MONGODB_DATABASE)
+    logr.info('params connecting string : ' + conn_string)
+    client = MongoClient(conn_string)
+    logr.info('db server info: ' + str(client.server_info()))
+    db = client[MONGODB_DATABASE]
+    coll = db[MONGODB_COLLECTION]
+    logr.info('document count: ' + str(coll.count()))
+    logr.info('document dump: ' + str(list(coll.find())))
     client.close()
 
-    
+
 if __name__ == '__main__':
     main()
