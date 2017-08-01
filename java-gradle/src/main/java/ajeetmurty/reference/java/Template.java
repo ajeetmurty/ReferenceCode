35ef4b5b71a72c0f9d80ec27f3dff0426be2998c
ajeet murty <ajeetmurty.dev@gmail.com>
2017-08-01 03:48:51 -0400

    java template with gradle support built in eclipse. initial commit.

---
+++ java-gradle/src/main/java/ajeetmurty/reference/java/Template.java
@@ -0,0 +1,31 @@
+package ajeetmurty.reference.java;
+
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Template {
+	private final Logger logp = LoggerFactory.getLogger(this.getClass().getName());
+
+	public static void main(String[] args) {
+		new Template();
+	}
+
+	public Template() {
+		doSomething();
+		// testing something
+	}
+
+	private void doSomething() {
+		logp.info("start");
+		try {
+			logp.info("doing info");
+			logp.debug("doing debug");
+			logp.error("doing error");
+			throw new IOException("throw io exception");
+		} catch (Exception e) {
+			logp.error(e.getMessage(), e);
+		}
+		logp.info("stop");
+	}
+}
